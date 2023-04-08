0539aead30162ad0966009881459dbe5144912ee
Sanket Panda <sanketpanda55.sp@gmail.com>
2023-04-08 21:15:05 +0530

    Updated api url

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://ems-api-j5v3.onrender.com/api',
+    baseUrl: 'https://spring-security-api.onrender.com/api',
     baseUrlDev: 'http://localhost:8000/api',
     signUp: '/auth/register',
     confirmAccount: '/auth/confirm-account',
